87db5fb632917efc5068e4f8db15a0a934645697
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-08 21:32:54 +0300

    COMMIT

--- Resume.cs
+++ Resume.cs
@@ -6,11 +6,11 @@ public class Resume
     public string _name;
     public List<Job> _jobs = new List<Job>();
 
-    // Method to display the resume
     public void Display()
     {
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
+        
         foreach (Job job in _jobs)
         {
             job.Display();
