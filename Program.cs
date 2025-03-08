dcfcc3c145e054c7dc0e17e1cd946952ef3e71fb
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-08 21:31:47 +0300

    COMMIT

--- Program.cs
+++ Program.cs
@@ -2,29 +2,33 @@ using System;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        // Create first job
+        // Create job instances
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
         job1._startYear = 2019;
         job1._endYear = 2022;
 
-        // Create second job
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
         job2._company = "Apple";
+        job2._jobTitle = "Manager";
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        // Create resume and assign jobs
+        // Create resume instance
         Resume myResume = new Resume();
         myResume._name = "Allison Rose";
+        
+        // Add jobs to resume
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
-        // Display the resume
+        // Verify access to job title
+        Console.WriteLine(myResume._jobs[0]._jobTitle);
+
+        // Display resume
         myResume.Display();
     }
 }
