245fdae9e7c7e590eb37b955c3b7ec593a3fea86
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-08 21:22:31 +0300

    commit

--- ResumeProject/obj/Debug/net10.0/ResumeProject.AssemblyInfo.cs
+++ ResumeProject/obj/Debug/net10.0/ResumeProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f65db1e63b42c79c8cb90144ffb999a23ae115e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b27337dbf495014f6553b0b57bb1c8bf41ababfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
